fad9c7304a22a109692e663c12e88898ab28fdfc
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-09 11:01:13 -0300

    usando map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Logo from './componentes/Logo'
 
+const textoOpcoes= ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
 function App() {
   return (
-    <div className='app'>
-      <header className='app-header'>
-       <Logo></Logo>
+    <div className="App">
+      <header className='App-header'>
+        <Logo></Logo>
+        <ul className='opcoes'>
+         {textoOpcoes.map((textodalista) => (
+          <li className='opcao'><p>{textodalista}</p></li>
+         ) ) }
+        </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
