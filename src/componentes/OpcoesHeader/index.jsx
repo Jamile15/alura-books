9a1bb88b724a7b547f2c4c430fb5ab113af4d4ac
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-24 09:14:29 -0300

    usando styled component

--- src/componentes/OpcoesHeader/index.jsx
+++ src/componentes/OpcoesHeader/index.jsx
@@ -1,14 +1,32 @@
-import './estilo.css'
-const textoOpcoes= ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+import styled from 'styled-components';
 
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
+
+const Opções = styled.ul`
+    display: flex;
+`
+
+const Opção = styled.li`
+     font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
 
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
+        <Opções>
             {textoOpcoes.map((textodalista) => (
-                <li className='opcao'><p>{textodalista}</p></li>
+                <Opção><p>{textodalista}</p></Opção>
             ))}
-        </ul>
+        </Opções>
     )
 }
 export default OpcoesHeader;
