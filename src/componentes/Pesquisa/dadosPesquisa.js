e5c8409dbd202f3c65930ce3a027e5186fb82ba5
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-25 10:27:25 -0300

    lista de livros

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,65 @@
+import livro from '../../imagens/livro.png'
+
+ export const livros = [
+    {
+        nome: 'Liderança em Design',
+        id: '1',
+        src: livro
+
+    },
+    {
+        nome: 'Receitas',
+        id: '2',
+        src: livro
+
+    },
+    {
+        nome: 'Bolos',
+        id: '3',
+        src: livro
+
+    },
+    {
+        nome: 'Codigo Limpo',
+        id: '4',
+        src: livro
+
+    },
+    {
+        nome: 'Arquitetura limpa',
+        id: '5',
+        src: livro
+
+    },
+    {
+        nome: 'Volta ao mundo',
+        id: '6',
+        src: livro
+
+    },
+    {
+        nome: 'Italia',
+        id: '7',
+        src: livro
+
+    },
+    {
+        nome: 'Mamamia',
+        id: '8',
+        src: livro
+
+    },
+    {
+        nome: 'Bolean',
+        id: '9',
+        src: livro
+
+    },
+    {
+        nome: 'Sabio',
+        id: '10',
+        src: livro
+
+    },
+]
+
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input"
 import styled from "styled-components"
 import { useState } from "react"
+import { livros } from "./dadosPesquisa"
 
 
 const PesquisaContainer = styled.section`
@@ -27,7 +28,8 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    console.log(livrosPesquisados)
 
 
     return (
@@ -36,13 +38,16 @@ function Pesquisa() {
             <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
             <Input
                 placeholder="Escreva sua proxima leitura"
-                onBlur={evento=> setTextoDigitado(evento.target.value)}
- 
+                onBlur={evento=> {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(banana => banana.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
                 />
 
-                <p>
-                    {textoDigitado}
-                </p>
+
+
+               
         </PesquisaContainer>
     )
 }
