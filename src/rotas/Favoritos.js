7b6fa51f173e836f2f8ca425a169ed3e40eae6c5
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-09-01 10:20:30 -0300

    favoritos

--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,16 +1,48 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Pesquisa from '../componentes/Pesquisa';
+import { getFavoritos } from '../servicos/favoritos';
+import { Titulo } from '../componentes/Titulo';
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`;
+
+const ResultadoContainer = styled.div`
+
+`
+
+const Resultado = styled.div`
 `
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([]);
+
+  async function fetchFavoritos() {
+    try {
+      const favoritosDaAPI = await getFavoritos();
+      console.log('Dados recebidos da API:', favoritosDaAPI);
+      setFavoritos([favoritosDaAPI]);
+    } catch (error) {
+      console.error('Erro ao buscar favoritos:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchFavoritos();
+  }, []);
+
   return (
     <AppContainer>
-      <Pesquisa/>
+      <Titulo>Aqui estão seus livros favoritos:</Titulo>
+      <ResultadoContainer>
+        {favoritos.map(favorito => (
+          <Resultado key={favorito.id}>
+            <p>{favorito.nome}</p>
+          </Resultado>
+        ))}
+      </ResultadoContainer>
     </AppContainer>
   );
 }
